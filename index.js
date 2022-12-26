1c31c05b5190e532e2bf28caeae000521e50a885
Leonard Chiu <leonardchiu@Leonards-Air.localdomain>
2022-12-26 21:34:07 +0800

    Worked on my MBA.. Pushing back to repo

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({ 
       headless: false,
-      executablePath: 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+      keepAlive: true
+      //executablePath: 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
      });
     const page = await browser.newPage();
-    await page.goto('https://online.apexpharma.com.sg/');
 
-    await page.type("input[name='username']", 'CLINCIAL');
+    await page.setViewport({
+    //width: page.viewport().width,
+    //height: page.viewport().height
+    width: 1366,
+    height: 768
+  });
+
+    await page.goto('https://online.apexpharma.com.sg/');
+    //await page.goto('https://completevms.com.sg/');
+
+    await page.type("input[name='username']", 'CLINICAL');
     await page.type("input[name='password']", '1313twl');
 
     await page.click("input[id='submit']");
+    
+    await page.waitForSelector('#promo_popup');
+    //await page.click("//div[@id='promo_popup']//button[@id='alertclose']")
 
-    await page.waitForSelector("a[href='/products']");
-    await page.click("a[href='/products']")
+    const popup = await page.evaluate(() => {
+      window.open('https://online.apexpharma.com.sg', 'popup');
+    });
+
+    await popup.waitForNavigation();
+
+    await popup.close();
+
+    await page.waitForSelector('#main_navigation_wrap');
+    //await page.click("a[href='/products']")
+    await page.click('#main_navigation_wrap')
   }
 )();
